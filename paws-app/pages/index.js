497a1c3a9cb4df42501cf59f93f934b56fc4c84e
Mae Mabanta <gmmabanta@gmail.com>
2022-10-16 18:15:34 +0800

    redirect to /mobile

--- paws-app/pages/index.js
+++ paws-app/pages/index.js
@@ -1,5 +1,11 @@
+import React, { useEffect } from "react";
+import Router from "next/router";
+
 export default function Home() {
-  return (
-    <div>Hello World!</div>
-  )
-}
+  useEffect(() => {
+    const { pathname } = Router;
+    if (pathname === "/") {
+      Router.push("/mobile");
+    }
+  });
+};
